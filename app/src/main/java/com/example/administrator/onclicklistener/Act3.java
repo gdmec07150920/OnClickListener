c5315e15ef770c98ec2615f5be7dc93c76da12ee
gdmec07150920 <15622116332@163.com>
2016-10-28 13:45:12 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/onclicklistener/Act3.java
@@ -0,0 +1,25 @@
+package com.example.administrator.onclicklistener;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Act3 extends AppCompatActivity {
+    private Button btn3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act3);
+        btn3= (Button) findViewById(R.id.button3);
+        btn3.setText("匿名内部类");
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Act3.this,"葵花宝典第三式",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/administrator/onclicklistener/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.administrator.onclicklistener;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn= (Button) findViewById(R.id.button);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this,"葵花宝典第一式",Toast.LENGTH_LONG);
+    }
+}
